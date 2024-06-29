8c3c21dc36cafa38b9f5404e9d4c98b96e2c6f66
kdcgovender <kayleize.govender@bbd.co.za>
2024-06-29 21:29:33 +0200

    remove update_date from financial info table

--- Database/flyway/migrations/V6__Create_FinancialInfo_Table.sql
+++ Database/flyway/migrations/V6__Create_FinancialInfo_Table.sql
@@ -2,7 +2,6 @@ CREATE TABLE FoodRetailMicroserviceSchema.[FinancialInfo]
 (
     financial_info_id INT PRIMARY KEY IDENTITY(1,1),
     property_name VARCHAR NOT NULL,
-    property_value VARCHAR NOT NULL,
-    updated_at DATETIME DEFAULT GETDATE()
+    property_value VARCHAR NOT NULL
 );
 GO
