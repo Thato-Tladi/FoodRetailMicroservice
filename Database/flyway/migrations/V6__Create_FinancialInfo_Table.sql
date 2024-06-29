616c9c293152690c3bc0d988bb463ca7bf66465a
kdcgovender <kayleize.govender@bbd.co.za>
2024-06-29 21:24:31 +0200

    update financial info table

--- Database/flyway/migrations/V6__Create_FinancialInfo_Table.sql
+++ Database/flyway/migrations/V6__Create_FinancialInfo_Table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE FoodRetailMicroserviceSchema.[FinancialInfo]
 (
     financial_info_id INT PRIMARY KEY IDENTITY(1,1),
-    current_price DOUBLE NOT NULL,
-    profit DOUBLE NOT NULL,
+    property_name VARCHAR NOT NULL,
+    property_value VARCHAR NOT NULL,
     updated_at DATETIME DEFAULT GETDATE()
 );
 GO
