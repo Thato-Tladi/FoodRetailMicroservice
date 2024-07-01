4e0b0fe3b4708d5f4b59bf315f9f0b5669b199c9
kdcgovender <kayleize.govender@bbd.co.za>
2024-07-01 18:46:24 +0200

    readd route for notFoundPage

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/ConsumerHistoryPage";
+import NotFoundPage from "./pages/NotfoundPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/Consumer History" element={<HomePage />} />
-      <Route path="*" element={<HomePage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
