19b70d89bcb5b3d7b293cb5bbe54fb30a464cefa
DerrykBBD <159010520+DerrykBBD@users.noreply.github.com>
2024-07-03 15:02:52 +0200

    Update css , improve mobile view and consumer history page

--- Frontend/src/pages/ConsumerHistoryPage.js
+++ Frontend/src/pages/ConsumerHistoryPage.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import BasicTable from '../components/BasicTable';
-import { getConsumerHistory } from '../api/api';
+import React, { useEffect, useState } from "react";
+import BasicTable from "../components/BasicTable";
+import { getConsumerHistory } from "../api/api";
 
 const columns = [
-  { id: 'ConsumerHistoryId', label: 'Consumer History ID' },
-  { id: 'ConsumerId', label: 'Consumer ID', align: 'right' },
-  { id: 'PurchasedDate', label: 'Date Of Purchase', align: 'right' },
-  { id: 'Price', label: 'Price (BBDough)', align: 'right' },
+  { id: "ConsumerHistoryId", label: "Consumer History ID" },
+  { id: "ConsumerId", label: "Consumer ID", align: "right" },
+  { id: "PurchasedDate", label: "Date Of Purchase", align: "right" },
+  { id: "Price", label: "Price (BBDough)", align: "right" },
 ];
 
 const ConsumerHistoryPage = () => {
@@ -17,15 +17,17 @@ const ConsumerHistoryPage = () => {
     const fetchConsumerHistory = async () => {
       try {
         const data = await getConsumerHistory();
-        const formattedData = data.map(item => ({
+        const formattedData = data.map((item) => ({
           ConsumerHistoryId: item.consumerHistoryId,
           ConsumerId: item.consumerId,
-          PurchasedDate: new Date(item.purchasedDate).toLocaleDateString('en-GB'),
+          PurchasedDate: new Date(item.purchasedDate).toLocaleDateString(
+            "en-GB"
+          ),
           Price: item.price,
         }));
         setRows(formattedData);
       } catch (error) {
-        console.error('Error fetching consumer history data:', error);
+        console.error("Error fetching consumer history data:", error);
       } finally {
         setLoading(false);
       }
@@ -35,7 +37,7 @@ const ConsumerHistoryPage = () => {
   }, []);
 
   return (
-    <div style={{ height: '80vh', overflowY: 'auto' }}>
+    <div>
       <h1>Consumer History</h1>
       <BasicTable columns={columns} rows={rows} loading={loading} />
     </div>
